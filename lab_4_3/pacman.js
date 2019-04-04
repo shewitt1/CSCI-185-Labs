0b40789d6c9e255c9e6324eec7a6b409960b81df
Spencer Hewitt <spencerhewitt@dhcp-39-28.mil.secure.int.wireless.unca.edu>
2019-04-04 00:07:59 -0400

    pacman can now eat pellets

---
+++ lab_4_3/pacman.js
@@ -0,0 +1,315 @@
+
+
+
+
+//pacman.js
+//global variables
+var pacman_position = [ 200, 200 ];
+var pacman_size = 100;
+var pacman_theta = 0.25*Math.PI;
+var theta_speed = 0.05*Math.PI;
+var pacman_direction = "right";
+var pacman_speed = 10;
+var score = 0;
+
+function game(){
+ var gameboard = document.getElementById("gameboard");
+ gameboard.innerHTML = "<canvas id=\"screen\" height=\"" +window.innerHeight +  "\" width=\"" + window.innerWidth + "\"></canvas>";
+ 
+ //start the animation
+ 
+animation_loop()
+}
+//handle keyboard events
+document.onkeydown = keypress;
+function keypress( e ){
+ //alert("e.keyCode=" + e.keyCode );
+ if( e.keyCode == 37){
+  //left arrow key
+  pacman_direction = "left";
+ }
+ else if( e.keyCode == 39){
+  //right arrow key 
+  pacman_direction = "right";
+  
+ }
+ else if( e.keyCode == 38){
+  //up arrow key
+  pacman_direction = "up";
+ }
+ else if( e.keyCode == 40){
+  //down arrow key 
+  pacman_direction = "down";
+ }
+}
+
+
+function animation_loop(){
+ //erase the screen
+ erase()
+ //make the background black
+ paint_background();
+ //draw pacman
+ move_pacman();
+ draw_pacman();
+ //draw ghosts
+ //draw walls
+ draw_all_walls();
+ //draw pellets
+ eat_pellets();
+ draw_all_pellets();
+ //draw cherries
+ 
+ 
+ 
+ //call animation_loop in a little bit
+ setTimeout(function(){ animation_loop(); }, 25);
+}
+//list of walls
+var horizontal_walls = [
+	[300,300],
+	[400,300],
+	[500,300],
+	[600,300],
+	[700,300],
+	[300,200],
+	[400,200],
+	[500,200],
+	[600,200],
+	[700,200],
+
+];
+var vertical_walls = [
+	[300,300],
+	[300,400],
+	[300,500],
+	[800,300],
+	[800,400],
+	[800,500],
+
+];
+
+function draw_all_walls(){
+ //draw all horizontal walls
+ for( var i=0 ; i < horizontal_walls.length ; i++){
+  //draw wall i 
+  draw_horizontal_wall( i );
+ }
+ 
+ //draw all veritcal walls
+ for ( var j=0 ; j < vertical_walls.length ; j++){
+  //draw wall j
+  draw_vertical_wall( j );
+ 
+ }
+}
+
+function draw_horizontal_wall( z ){
+ //z is the index of the wall we are drawing
+ var c = document.getElementById("screen");
+ var ctx = c.getContext("2d");
+ //upper left corner on the wall
+ var this_wall = horizontal_walls[z];
+ var x = this_wall[0];
+ var y = this_wall[1];
+ //draw wall
+ ctx.fillStyle = "darkblue";
+ ctx.fillRect(x, y, pacman_size, 10) 
+}
+
+function draw_vertical_wall( d ){
+ //z is the index of the wall we are drawing
+ var c = document.getElementById("screen");
+ var ctx = c.getContext("2d");
+ 
+ var this_wall = vertical_walls[d];
+ var x = this_wall[0];
+ var y = this_wall[1];
+ 
+ ctx.fillStyle = "darkblue";
+ ctx.fillRect(x, y, 10, pacman_size)
+}
+
+var the_pellets = [
+	[200,250],
+	[300,250],
+	[400,250],
+	[500,250],
+	[600,250],
+	[700,250],
+	[800,250],
+	[900,250],
+	[900,350],
+	[900,450],
+
+]
+
+function eat_pellets() {
+	for(var i=0; i < the_pellets.length ; i++){
+		// check if pacman collied with pellet i
+		var x = the_pellets[i][0];
+		var y = the_pellets[i][1];
+		
+		if( pacman_collide_pellet(x,y) ){
+			// pacman ate the pellet
+			// remove pellet i
+			the_pellets.splice(i,1);
+			// since we shorted the list by 1, we decriment the counter by 1
+			i--;
+
+			score++;
+			console.log(score);
+		}
+	}
+}
+
+function pacman_collide_pellet( pellet_x, pellet_y ){
+	// check collison with pellet
+	// check for collision in the vertical
+	var A_left = pacman_position[0] - pacman_size/2;
+	var A_right = pacman_position[0] + pacman_size/2;
+	var B_left = pellet_x - 5;
+	var B_right = pellet_x + 5;
+	
+	var A_top = pacman_position[1] - pacman_size/2;
+	var A_bottom = pacman_position[1] + pacman_size/2;
+	var B_top = pellet_y - 5;
+	var B_bottom = pellet_y + 5;
+	
+	if( 
+	   (A_right > B_left && A_left < B_left)
+	      || // "or"
+	   (A_right > B_right && A_left < B_right)
+	  ){
+		// B_left inside A
+		if(B_top < A_bottom && B_top > A_top){
+			// B_top inside A
+			return true;
+		}
+		if(B_bottom < A_bottom && B_bottom > A_top){
+			// B_bottom inside A
+			return true;
+		}
+	}
+	
+	// no collision
+	return false;
+	
+}
+
+
+
+
+function draw_all_pellets(){
+ for( var l=0 ; l < the_pellets.length ; l++){
+  draw_pellets( l );
+ }
+}
+function draw_pellets( p ){
+ var c = document.getElementById("screen");
+ var ctx = c.getContext("2d");
+ 
+
+ var this_pellet = the_pellets[p];
+ var x = this_pellet[0];
+ var y = this_pellet[1];
+ 
+ ctx.beginPath();
+ ctx.fillStyle = "white";
+ ctx.arc(x, y, 10, 0, 2 * Math.PI);
+ ctx.fill()
+ ctx.closePath();
+}
+
+
+function move_pacman(){
+ if( pacman_direction == "right"){
+  pacman_position[0] = pacman_position[0] + pacman_speed;
+  if(pacman_position[0] > window.innerWidth ){
+   //wrap around to the left
+   pacman_position[0] = 0;
+  }
+ }
+ else if(pacman_direction == "left"){
+  pacman_position[0] = pacman_position[0] - pacman_speed;
+  if(pacman_position[0] <= 0){
+   pacman_position[0] = window.innerWidth;
+  }
+ }
+ else if(pacman_direction == "up"){
+  pacman_position[1] = pacman_position[1] - pacman_speed;
+  if(pacman_position[1] <= 0){
+   pacman_position[1] = window.innerHeight;
+  }
+ }
+ else if( pacman_direction == "down"){
+  pacman_position[1] = pacman_position[1] + pacman_speed;
+  if(pacman_position[1] > window.innerHeight){
+    pacman_position[1] = 0;
+  }
+ }
+}
+
+function draw_pacman(){
+ var c = document.getElementById("screen");
+ var ctx = c.getContext("2d");
+ 
+ var x = pacman_position[0];
+ var y = pacman_position[1];
+ 
+ //check what direction pacman is facing
+ var rotation_offset = 0;
+ 
+ if(pacman_direction == "right"){
+  
+ }
+ else if(pacman_direction == "left"){
+  rotation_offset = Math.PI;
+ }
+ else if(pacman_direction == "up"){
+  rotation_offset = -1*Math.PI/2;
+ }
+ else if(pacman_direction == "down"){
+  rotation_offset = Math.PI/2;
+ }
+ 
+ ctx.beginPath();
+ ctx.arc(x, y, pacman_size/2, pacman_theta + rotation_offset, (2*Math.PI - pacman_theta) + rotation_offset);
+ ctx.lineTo(x, y);
+ 
+ var delta_x = (pacman_size/2) *Math.cos(pacman_theta + rotation_offset);
+ var delta_y = (pacman_size/2) *Math.sin(pacman_theta + rotation_offset);
+ ctx.lineTo(x + delta_x , y + delta_y);
+ ctx.closePath();
+ //color in pacman
+ ctx.fillStyle = "yellow";
+ ctx.fill();
+ 
+ //close mouth
+ pacman_theta = pacman_theta - theta_speed;
+ //open mouth
+ if (pacman_theta <= 0){
+  theta_speed = theta_speed*-1;
+ }
+ else if (pacman_theta >= 0.25*Math.PI){
+  theta_speed = theta_speed * -1;
+ }
+}
+
+function erase(){
+ var c = document.getElementById("screen");
+ var ctx = c.getContext("2d");
+ 
+ ctx.clearRect(0, 0, c.width, c.height);
+}
+
+
+function paint_background(){
+ var c = document.getElementById("screen");
+ var ctx = c.getContext("2d");
+ 
+ ctx.fillStyle = "black"
+ ctx.fillRect(0, 0, c.width, c.height)
+ 
+ 
+}
