7f63d0af3b1eda980da82bc8f998a73271b1eee2
Spencer Hewitt <spencerhewitt@dhcp-39-28.mil.secure.int.wireless.unca.edu>
2019-04-07 22:19:14 -0400

    check if pacman collides with wall

--- lab_4_3/pacman.js
+++ lab_4_3/pacman.js
@@ -4,7 +4,7 @@
 
 //pacman.js
 //global variables
-var pacman_position = [ 200, 200 ];
+var pacman_position = [ 50, 50 ];
 var pacman_size = 100;
 var pacman_theta = 0.25*Math.PI;
 var theta_speed = 0.05*Math.PI;
@@ -57,6 +57,7 @@ function animation_loop(){
  draw_all_walls();
  //draw pellets
  eat_pellets();
+ pacman_collide_wall();
  draw_all_pellets();
  //draw cherries
  
@@ -88,6 +89,20 @@ var vertical_walls = [
 	[800,500],
 
 ];
+
+var the_pellets = [
+	[200,250],
+	[300,250],
+	[400,250],
+	[500,250],
+	[600,250],
+	[700,250],
+	[800,250],
+	[900,250],
+	[900,350],
+	[900,450],
+
+]
 
 function draw_all_walls(){
  //draw all horizontal walls
@@ -130,20 +145,6 @@ function draw_vertical_wall( d ){
  ctx.fillRect(x, y, 10, pacman_size)
 }
 
-var the_pellets = [
-	[200,250],
-	[300,250],
-	[400,250],
-	[500,250],
-	[600,250],
-	[700,250],
-	[800,250],
-	[900,250],
-	[900,350],
-	[900,450],
-
-]
-
 function eat_pellets() {
 	for(var i=0; i < the_pellets.length ; i++){
 		// check if pacman collied with pellet i
@@ -158,7 +159,11 @@ function eat_pellets() {
 			i--;
 
 			score++;
-			console.log(score);
+			document.getElementById('scorenum').innerHTML = score;
+
+			if (the_pellets == 0) {
+				alert('You Win!')
+			}
 		}
 	}
 }
@@ -248,6 +253,38 @@ function move_pacman(){
     pacman_position[1] = 0;
   }
  }
+
+ 	// check if pacman hit a wall
+		// check horizontal walls
+		for(var q=0; q < horizontal_walls.length; q++){
+		
+			var x = horizontal_walls[q][0];
+			var y = horizontal_walls[q][1];
+			
+			if( pacman_collide_wall(x,y,'horizontal') ){
+				// if so, move pacman back to saved position
+				pacman_position[0] = save_x;
+				pacman_position[1] = save_y;
+				// stop the loop
+				break;
+			}
+		}
+			// check vertidcal walls
+		for(var q=0; q < vertical_walls.length; q++){
+			var x = vertical_walls[q][0];
+			var y = vertical_walls[q][1];
+			
+			if( pacman_collide_wall(x,y,'vertical') ){
+				// if so, move pacman back to saved position
+				pacman_position[0] = save_x;
+				pacman_position[1] = save_y;
+				// stop the loop
+				break;
+			}
+		}
+		
+		
+		
 }
 
 function draw_pacman(){
@@ -294,6 +331,58 @@ function draw_pacman(){
  else if (pacman_theta >= 0.25*Math.PI){
   theta_speed = theta_speed * -1;
  }
+}
+
+function pacman_collide_wall( wall_x, wall_y, direction) {
+	// check collison with pellet
+	// check for collision in the vertical
+	var A_left = pacman_position[0] - pacman_size/2;
+	var A_right = pacman_position[0] + pacman_size/2;	
+	var A_top = pacman_position[1] - pacman_size/2;
+	var A_bottom = pacman_position[1] + pacman_size/2;
+	
+	var B_top = wall_y;
+	var B_left = wall_x;
+	if( direction == 'horizontal'){
+		var B_bottom = wall_y + 10;
+		var B_right = wall_x + pacman_size;
+	}else if( direction == 'vertical'){
+		var B_bottom = wall_y + pacman_size;
+		var B_right = wall_x + 10;
+	}
+	// Check if A collides with B
+	if( 
+	   (A_right > B_left && A_left < B_left)
+	      || // "or"
+	   (A_right > B_right && A_left < B_right)
+	  ){
+		// B_left inside A
+		if(B_top < A_bottom && B_top > A_top){
+			// B_top inside A
+			return true;
+		}
+		if(B_bottom < A_bottom && B_bottom > A_top){
+			// B_bottom inside A
+			return true;
+		}
+	}
+	// Check if B collies with A
+	if(
+		(B_right > A_left && B_left < A_left)
+			|| // or
+		(B_right > A_right && B_left < A_right)
+	  ){
+		if(A_top < B_bottom && A_top > B_top){
+			return true;
+		}
+		if(A_bottom < B_bottom && A_bottom > B_top){
+			return true;
+		}
+	}
+	
+	
+	// no collision
+	return false;
 }
 
 function erase(){
