0145b5049f0b1bab15c3c9628f38e7571d4f6948
Spencer Hewitt <spencerhewitt@dhcp-38-179.mil.secure.int.wireless.unca.edu>
2019-04-22 23:40:47 -0400

    added final project

---
+++ quiz_4_17/tictactoe.js
@@ -0,0 +1,23 @@
+/* tictactoe.js */
+
+function change_color() {
+
+    var player_x = document.getElementById('player1');
+var player_y = document.getElementById('player2');
+
+    document.getElementById('player1').onclick = function () {
+        player_x.style.backgroundColor == "yellow";
+        
+
+        
+    }
+}
+
+
+function select_square() {
+    var NW = document.getElementById('north-west');
+        NW.innerHTML == "<i class=\"fa fa-circle-o fa-5x\" aria-hidden=\"true\">";
+    
+}
+
+
